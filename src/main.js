9f4fac4e08c87fe44ae8c75e483cfd4f65e3556c
Erik Watson <championchap@gmail.com>
2021-01-27 17:22:50 +0000

    added some bad shading to the scene

--- src/main.js
+++ src/main.js
@@ -33,6 +33,26 @@ const hero = {
   position: vec2.create(32, 32)
 }
 
+function shadeColor(color, percent) {
+  let R = parseInt(color.substring(1,3),16);
+  let G = parseInt(color.substring(3,5),16);
+  let B = parseInt(color.substring(5,7),16);
+
+  R = parseInt(R * (100 + percent) / 100);
+  G = parseInt(G * (100 + percent) / 100);
+  B = parseInt(B * (100 + percent) / 100);
+
+  R = (R < 255) ? R : 255;  
+  G = (G < 255) ? G : 255;  
+  B = (B < 255) ? B : 255;  
+
+  let RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+  let GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+  let BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+  return `#${RR}${GG}${BB}`;
+}
+
 function toRadians (degrees) {
   return degrees * (Math.PI / 180)
 }
@@ -129,7 +149,10 @@ g.setRender(gfx => {
     collision = collision.collision
 
     var height = canvasSize.height - (canvasSize.height * collision.timeOfCollision)
-    let colour = (rect.type === 2) ? 'purple' : 'yellow'
+    let colour = (rect.type === 2) ? '#800080' : '#FFEF00'
+
+    const shade = Math.round(collision.timeOfCollision * 100)
+    colour = shadeColor(colour, 50 - shade)
 
     gfx.rect(
       { x: i * columnWidth, y: (canvasSize.height - height) / 2 },
@@ -152,7 +175,7 @@ g.setRender(gfx => {
           gfx.square(
             { x: x * tileSize, y: y * tileSize }, 
             tileSize,
-            { fill: { colour: 'yellow' } }
+            { fill: { colour: '#FFEF00' } }
           )
           break
 
@@ -160,7 +183,7 @@ g.setRender(gfx => {
           gfx.square(
             { x: x * tileSize, y: y * tileSize }, 
             tileSize,
-            { fill: { colour: 'purple' } }
+            { fill: { colour: '#800080' } }
           )
           break
 
